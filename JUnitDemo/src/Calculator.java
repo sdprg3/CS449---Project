c159871addf5d88513dd5adca11a7349e7191739
sdprg3 <113368129+sdprg3@users.noreply.github.com>
2024-09-06 23:50:13 -0500

    Add files via upload

---
+++ JUnitDemo/src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    /**
+     * Adds two numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+      return a + b;
+    }
+  
+    /**
+     * Multiplies two numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the product of a and b
+     */
+    public int multiply(int a, int b) {
+      return a * b;
+    }
+  }
+  
