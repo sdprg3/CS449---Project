b6727291ff906ccc60cd0c270a712df35ddca1cb
sdprg3 <113368129+sdprg3@users.noreply.github.com>
2024-09-06 21:02:02 -0500

    Add files via upload

---
+++ Sprint0/src/Controller.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+public class Controller {
+    
+    @FXML
+    private ToggleGroup Blue;
+
+    @FXML
+    private ToggleGroup Blue1;
+
+    @FXML
+    private ToggleGroup Gamemode;
+
+    @FXML
+    private ToggleGroup Red;
+
+    @FXML
+    private ToggleGroup Red1;
+
+    @FXML
+    private RadioButton rButton1;
+
+
+    @FXML
+    void choose_player_type(ActionEvent event) {
+
+    }
+
+}
