c4343b5a1cd2cad70cedbd3c546a588eddaea6a9
sdprg3 <113368129+sdprg3@users.noreply.github.com>
2024-09-06 23:53:52 -0500

    Update Main.java

--- Sprint0/src/Main.java
+++ Sprint0/src/Main.java
@@ -1,4 +1,3 @@
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainscene.fxml"));
